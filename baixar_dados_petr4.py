730b1356cb175cc297841edff3e15f8d8817b262
Leonardo Gaspar Saheb <144124073+Leonardo-Gaspar@users.noreply.github.com>
2026-05-14 13:11:21 -0300

    Update Entrega 3 baixar_dados_petr4.py

--- baixar_dados_petr4.py
+++ baixar_dados_petr4.py
@@ -1,9 +1,20 @@
 import yfinance as yf
 
-df = yf.download('PETR4.SA', start='2000-01-01', end='2025-02-09')
+TICKER = 'PETR4.SA'
+DATA_INICIAL = '2000-01-01'
+DATA_FINAL = '2025-02-09'
+
+print("Baixando dados da PETR4...")
+
+df = yf.download(
+    TICKER,
+    start=DATA_INICIAL,
+    end=DATA_FINAL
+)
 
 df.reset_index(inplace=True)
 
 df.to_csv('petr4.csv', index=False)
 
 print("CSV gerado com sucesso!")
+print(f"Total de registros: {len(df)}")
